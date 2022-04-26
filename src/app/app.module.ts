3ad8d68b48dfc5858a225a682fc9f7c789a6f0cd
Killer-kate <75667457+Killer-kate@users.noreply.github.com>
2022-04-26 21:26:18 +0300

    create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from "@angular/platform-browser"
 
 import { AppComponent } from "./app.component"
 import { PlaceEditDialogModule } from "./ui/place-edit-dialog/place-edit-dialog.module"
-import { ShellModule } from "./ui/shell/shell.module"
+import { ShellModule } from "./ui/shell/shell.module";
+import { MdEditorComponent } from './ui/shared/components/md-editor/md-editor.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MdEditorComponent
   ],
   imports: [
     BrowserModule,
